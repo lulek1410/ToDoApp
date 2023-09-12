fb8457f1f5e3bf02a61069f013c6907473b53684
lulek1410 <jan.szewczynski@gmail.com>
2023-09-12 18:59:29 +0200

    Add delete task functionality

--- src/components/Main/Card.tsx
+++ src/components/Main/Card.tsx
@@ -1,15 +1,30 @@
 import "./Card.css";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { deleteTask } from "../../store/reducers/tasksSlice";
 
 interface CardProps {
+	id: number;
 	title: string;
 	description: string;
 	dueDate: string;
 }
 
-const Card = ({ title, description, dueDate }: CardProps) => {
+const Card = ({ id, title, description, dueDate }: CardProps) => {
+	const dispatch = useAppDispatch();
+
 	return (
 		<div className="card">
+			<button
+				className="icon-btn"
+				onClick={() => {
+					dispatch(deleteTask(id));
+				}}
+			>
+				<FontAwesomeIcon icon={faTrash} />
+			</button>
 			<div className="card-title">
 				<h2>{title}</h2>
 				<p>{dueDate}</p>
--- src/store/reducers/tasksSlice.ts
+++ src/store/reducers/tasksSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface Task {
+	id: number;
 	title: string;
 	description: string;
 	dueDate: string;
@@ -13,16 +14,19 @@ interface TasksState {
 const initialState: TasksState = {
 	tasks: [
 		{
+			id: 1,
 			title: "Task-1",
 			description: "description",
 			dueDate: "2024-09-30",
 		},
 		{
+			id: 2,
 			title: "Task-2",
 			description: "description 2",
 			dueDate: "2023-12-30",
 		},
 		{
+			id: 3,
 			title: "Task-3",
 			description: "description 3",
 			dueDate: "2023-10-12",
@@ -38,7 +42,10 @@ export const tasksSlice = createSlice({
 			state.tasks.push(action.payload);
 		},
 		editTask: (state) => {},
-		deleteTask: (state) => {},
+		deleteTask: (state, action: PayloadAction<number>) => {
+			const index = state.tasks.findIndex((task) => task.id === action.payload);
+			state.tasks.splice(index, 1);
+		},
 	},
 });
 
