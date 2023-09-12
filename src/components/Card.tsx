71dc4397389eb5c81bcf328407c3332338a7a70f
lulek1410 <jan.szewczynski@gmail.com>
2023-09-12 17:15:01 +0200

    change name to title

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,16 +2,16 @@ import "./Card.css";
 import React from "react";
 
 interface CardProps {
-	name: string;
+	title: string;
 	description: string;
 	dueDate: string;
 }
 
-const Card = ({ name, description, dueDate }: CardProps) => {
+const Card = ({ title, description, dueDate }: CardProps) => {
 	return (
 		<div className="card">
 			<div className="card-title">
-				<h2>{name}</h2>
+				<h2>{title}</h2>
 				<p>{dueDate}</p>
 			</div>
 			<p>{description}</p>
