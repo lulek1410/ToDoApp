be2f9876acb4638a812f270a1895b674eea26e6b
lulek1410 <jan.szewczynski@gmail.com>
2023-09-26 12:51:49 +0200

    refactor of sorting

--- src/hooks/useFilteredTasks.tsx
+++ src/hooks/useFilteredTasks.tsx
@@ -1,26 +1,28 @@
+import Task from "../store/reducers/interfaces/Task";
 import { useAppSelector } from "./useAppSelector";
 
 const useFilteredTasks = () => {
 	const tasks = useAppSelector((state) => state.tasks);
 	const filter = useAppSelector((state) => state.aside.selectedFilter);
+	const isOverdue = (task: Task) => {
+		return task.overdueDays > 0;
+	};
 
 	switch (filter) {
 		case "All": {
 			return [...tasks].sort((a, b) => {
-				if ((a.done && !b.done) || (a.overdueDays <= 0 && b.overdueDays > 0))
-					return 1;
-				if ((!a.done && b.done) || (a.overdueDays > 0 && b.overdueDays <= 0))
-					return -1;
+				if ((a.done && !b.done) || (!isOverdue(a) && isOverdue(b))) return 1;
+				if ((!a.done && b.done) || (isOverdue(a) && !isOverdue(b))) return -1;
 				return 0;
 			});
 			break;
 		}
 		case "To do": {
-			return tasks.filter((task) => task.overdueDays <= 0 && !task.done);
+			return tasks.filter((task) => !isOverdue(task) && !task.done);
 			break;
 		}
 		case "Overdue": {
-			return tasks.filter((task) => task.overdueDays > 0 && !task.done);
+			return tasks.filter((task) => isOverdue(task) && !task.done);
 			break;
 		}
 		case "Done": {
