15f33a1debb2c54f52c8954c6442f580bfb37440
lulek1410 <jan.szewczynski@gmail.com>
2023-09-27 19:04:56 +0200

    remove client files from server branch

--- client/src/utils/getDatesDiffInDays.ts
+++
@@ -1,6 +0,0 @@
-export const getDatesDiffInDays = (start: string, end: string) => {
-	const dateStart = new Date(start);
-	const dateEnd = new Date(end);
-	const diffTime = dateEnd.getTime() - dateStart.getTime();
-	return Math.round(diffTime / (1000 * 60 * 60 * 24));
-};
